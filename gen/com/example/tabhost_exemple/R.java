88ba56f87b0c53cd9919ba1164d1dece5ae27aa8
Serg0 <sernad@mail.ru>
2012-12-09 23:01:34 +0200

    second commit

--- gen/com/example/tabhost_exemple/R.java
+++ gen/com/example/tabhost_exemple/R.java
@@ -27,13 +27,15 @@ public final class R {
         public static final int button1=0x7f070001;
         public static final int button2=0x7f070002;
         public static final int button3=0x7f070003;
-        public static final int imageView1=0x7f070004;
-        public static final int menu_settings=0x7f070009;
+        public static final int button4=0x7f070004;
+        public static final int imageView1=0x7f070006;
+        public static final int menu_settings=0x7f07000b;
         public static final int textView1=0x7f070000;
-        public static final int textView2=0x7f070005;
-        public static final int textView3=0x7f070006;
-        public static final int textView4=0x7f070007;
-        public static final int text_view=0x7f070008;
+        public static final int textView2=0x7f070007;
+        public static final int textView3=0x7f070008;
+        public static final int textView4=0x7f070009;
+        public static final int textViewMain=0x7f070005;
+        public static final int text_view=0x7f07000a;
     }
     public static final class layout {
         public static final int a=0x7f030000;
@@ -51,6 +53,9 @@ public final class R {
         public static final int app_name=0x7f040000;
         public static final int dialog1=0x7f040004;
         public static final int dialog1_title=0x7f040005;
+        public static final int dialog2=0x7f040006;
+        public static final int dialog3=0x7f040007;
+        public static final int dialog4=0x7f040008;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
         public static final int title_activity_main=0x7f040003;
--- src/com/example/tabhost_exemple/MainActivity.java
+++ src/com/example/tabhost_exemple/MainActivity.java
@@ -5,6 +5,7 @@ import java.util.List;
 import android.content.DialogInterface;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.AlertDialog.Builder;
 import android.app.TabActivity;
@@ -24,200 +25,267 @@ import android.widget.TabHost;
 import android.widget.Toast;
 import android.widget.TabHost.TabSpec;
 import android.widget.TextView;
-import android.widget.TabHost;
+import java.util.Calendar;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import java.text.DateFormat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
-public class MainActivity extends Activity  {
-public static MainActivity instance;
-private Dialog myDialog;
-final Context context = this;
-Intent intent;
-@Override
-public void onConfigurationChanged(Configuration newConfig) {
+public class MainActivity extends Activity {
+	public static MainActivity instance;
+	private Dialog myDialog;
+	final Context context = this;
+	Intent intent;
+	TextView text;
+	DateFormat formatDateTime=DateFormat.getDateTimeInstance();
+	final Calendar dateTime=Calendar.getInstance();
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
 
+		super.onConfigurationChanged(newConfig);
+	}
 
-    super.onConfigurationChanged(newConfig);
-}
-@Override
-protected void onSaveInstanceState(Bundle outState) {
-    super.onSaveInstanceState(outState);
-    
-}
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
 
-@Override
-protected void onRestoreInstanceState(Bundle savedInstanceState) {
-    super.onRestoreInstanceState(savedInstanceState);
-}
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_main2);
-//        Button a = (Button) findViewById(R.id.button1);
-//        a.setText("Show TABS");
-//        a.setOnClickListener(new OnClickListener() {
-//			@Override
-//			public void onClick(View v) {
-//				  getTabs();				
-//				  
-//			}
-//		});
-//        Button b = (Button) findViewById(R.id.button2);
-//        b.setText("ViewPager");
-//        b.setOnClickListener(new OnClickListener() {
-//			@Override
-//			public void onClick(View v) {
-//				setViewPager();				
-//			}
-//		});
-//        
-    
-//      setViewPager();
-//        Builder AD = new AlertDialog.Builder(this);
-        findViewById(R.id.button1).setOnClickListener(new OnClickListener() {
-			
+	}
+
+	@Override
+	protected void onRestoreInstanceState(Bundle savedInstanceState) {
+		super.onRestoreInstanceState(savedInstanceState);
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_main2);
+		
+		
+		text = (TextView)findViewById(R.id.textViewMain);
+		
+		
+		// Button a = (Button) findViewById(R.id.button1);
+		// a.setText("Show TABS");
+		// a.setOnClickListener(new OnClickListener() {
+		// @Override
+		// public void onClick(View v) {
+		// getTabs();
+		//
+		// }
+		// });
+		// Button b = (Button) findViewById(R.id.button2);
+		// b.setText("ViewPager");
+		// b.setOnClickListener(new OnClickListener() {
+		// @Override
+		// public void onClick(View v) {
+		// setViewPager();
+		// }
+		// });
+		//
+
+		// setViewPager();
+		// Builder AD = new AlertDialog.Builder(this);
+		findViewById(R.id.button1).setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
 				AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
 						context);
 				alertDialogBuilder.setTitle(R.string.dialog1_title);
-				View view = LayoutInflater.from(context).inflate(R.layout.custom_layout, null );
+				View view = LayoutInflater.from(context).inflate(
+						R.layout.custom_layout, null);
 				alertDialogBuilder.setView(view);
-				alertDialogBuilder.setCancelable(false)
-				.setPositiveButton("Yes",new DialogInterface.OnClickListener() {
-					public void onClick(DialogInterface dialog,int id) {
-						// if this button is clicked, close
-						// current activity
-						MainActivity.this.finish();
-					}
-				  })
-				.setNegativeButton("No",new DialogInterface.OnClickListener() {
-					public void onClick(DialogInterface dialog,int id) {
-						// if this button is clicked, just close
-						// the dialog box and do nothing
-						dialog.cancel();
-					}
-				})
-				.setNeutralButton("%)", new DialogInterface.OnClickListener() {
-					
-					@Override
-					public void onClick(DialogInterface dialog, int which) {
-						// TODO Auto-generated method stub
-//						Toast.makeText(context, "ololo", Toast.LENGTH_LONG).show();
-//						dialog.
-						
-					}
-				});
-//				alertDialogBuilder.setCancelable(true);
-				 AlertDialog alertDialog = alertDialogBuilder.create();
+				alertDialogBuilder
+						.setCancelable(false)
+						.setPositiveButton("Yes",
+								new DialogInterface.OnClickListener() {
+									public void onClick(DialogInterface dialog,
+											int id) {
+										// if this button is clicked, close
+										// current activity
+										MainActivity.this.finish();
+									}
+								})
+						.setNegativeButton("No",
+								new DialogInterface.OnClickListener() {
+									public void onClick(DialogInterface dialog,
+											int id) {
+										// if this button is clicked, just close
+										// the dialog box and do nothing
+										dialog.cancel();
+									}
+								})
+						.setNeutralButton("%)",
+								new DialogInterface.OnClickListener() {
 
-				    alertDialog.show();
-				
+									@Override
+									public void onClick(DialogInterface dialog,
+											int which) {
+										// TODO Auto-generated method stub
+										// Toast.makeText(context, "ololo",
+										// Toast.LENGTH_LONG).show();
+										// dialog.
+
+									}
+								});
+				// alertDialogBuilder.setCancelable(true);
+				AlertDialog alertDialog = alertDialogBuilder.create();
+
+				alertDialog.show();
+
 			}
 		});
-        
-        findViewById(R.id.button2).setOnClickListener(new OnClickListener() {
-			
+
+		findViewById(R.id.button2).setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
-//				AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
-//						context);
-//				alertDialogBuilder.setTitle(R.string.dialog1_title);
-//				View view = LayoutInflater.from(context).inflate(R.layout.custom_layout, null );
-//				alertDialogBuilder.setView(view);
-////				alertDialogBuilder.setCancelable(true);
-//				 AlertDialog alertDialog = alertDialogBuilder.create();
-//				 
-//
-//				    alertDialog.show();
-				
+
 				intent = new Intent(MainActivity.this, Tab_Activity.class);
 				startActivity(intent);
-//				
 			}
 		});
-        
-    instance = this;
-    }
-    
-    
+		findViewById(R.id.button3).setOnClickListener(new OnClickListener() {
 
-    public static MainActivity getInstance() {
+			@Override
+			public void onClick(View v) {
+				new DatePickerDialog(MainActivity.this, (new DatePickerDialog.OnDateSetListener() {
+				
+				@Override
+				public void onDateSet(DatePicker view, int year, int monthOfYear,
+						int dayOfMonth) {
+					dateTime.set(Calendar.YEAR,year);
+					dateTime.set(Calendar.MONTH, monthOfYear);
+					dateTime.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+					updateTitle();
+					
+				}
+			}),
+					dateTime.get(Calendar.YEAR), dateTime
+							.get(Calendar.MONTH), dateTime
+							.get(Calendar.DAY_OF_MONTH)).show();
+		
+				
+			}
+		});
+		
+		findViewById(R.id.button4).setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				new TimePickerDialog(MainActivity.this, (new TimePickerDialog.OnTimeSetListener() {
+					
+					@Override
+					public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+						dateTime.set(Calendar.HOUR_OF_DAY, hourOfDay);
+						dateTime.set(Calendar.MINUTE,minute);
+						updateTitle();
+						
+					}
+				}), dateTime.get(Calendar.HOUR_OF_DAY), dateTime.get(Calendar.MINUTE), true).show();
+			}
+		});
+
+
+		instance = this;
+	}
+public void updateTitle() {
+	// TODO Auto-generated method stub
+ text.setText(formatDateTime.format(dateTime.getTime()));
+
+}
+	@Override
+	@Deprecated
+	protected Dialog onCreateDialog(int id) {
+		// TODO Auto-generated method stub
+		return super.onCreateDialog(id);
+	}
+
+	public static MainActivity getInstance() {
 		return instance;
 	}
-    
-    
-    
-    private void setViewPager() {
+
+	private void setViewPager() {
 		// TODO Auto-generated method stub
-    	 LayoutInflater inflater = LayoutInflater.from(this);
-         List<View> pages = new ArrayList<View>();
-         
-         View page = inflater.inflate(R.layout.page, null);
-         TextView textView = (TextView) page.findViewById(R.id.text_view);
-         textView.setText("Страница 1");
-         pages.add(page);
-         
-         page = inflater.inflate(R.layout.page, null);
-         textView = (TextView) page.findViewById(R.id.text_view);
-         textView.setText("Страница 2");
-         pages.add(page);
-         
-         page = inflater.inflate(R.layout.page, null);
-         textView = (TextView) page.findViewById(R.id.text_view);
-         textView.setText("Страница 3");
-         pages.add(page);
-         textView.setText("!!!!!!!!!!");
-         
-         SamplePagerAdapter pagerAdapter = new SamplePagerAdapter(pages);
-         ViewPager viewPager = new ViewPager(this);
-         viewPager.setAdapter(pagerAdapter);
-         viewPager.setCurrentItem(1);     
-         
-         setContentView(viewPager);
-         
-         
+		LayoutInflater inflater = LayoutInflater.from(this);
+		List<View> pages = new ArrayList<View>();
+
+		View page = inflater.inflate(R.layout.page, null);
+		TextView textView = (TextView) page.findViewById(R.id.text_view);
+		textView.setText("Страница 1");
+		pages.add(page);
+
+		page = inflater.inflate(R.layout.page, null);
+		textView = (TextView) page.findViewById(R.id.text_view);
+		textView.setText("Страница 2");
+		pages.add(page);
+
+		page = inflater.inflate(R.layout.page, null);
+		textView = (TextView) page.findViewById(R.id.text_view);
+		textView.setText("Страница 3");
+		pages.add(page);
+		textView.setText("!!!!!!!!!!");
+
+		SamplePagerAdapter pagerAdapter = new SamplePagerAdapter(pages);
+		ViewPager viewPager = new ViewPager(this);
+		viewPager.setAdapter(pagerAdapter);
+		viewPager.setCurrentItem(1);
+
+		setContentView(viewPager);
+
 	}
-//    public  void getTabs() {
-//    	// TODO Auto-generated method stub
-//    	
-//        TabHost tabHost = getTabHost();	
-//        
-//        // Tab for Photos
-//        TabSpec photospec = tabHost.newTabSpec("A");
-//        // setting Title and Icon for the Tab
-//        photospec.setIndicator("Photos", getResources().getDrawable(R.drawable.icon_photos_tab));
-//        Intent photosIntent = new Intent(this, A_activity.class);
-//        Log.d("LOG", "CurrentTab "+getTabHost().getCurrentTab());
-//        photosIntent.putExtra("position", getTabHost().getTabWidget().getChildCount());
-//        photospec.setContent(photosIntent);
-//        
-//        // Tab for Songs
-//        TabSpec songspec = tabHost.newTabSpec("B");
-//        songspec.setIndicator("Songs", getResources().getDrawable(R.drawable.icon_songs_tab));
-//        Intent songsIntent = new Intent(this, B_activity.class);
-//        songsIntent.putExtra("position", getTabHost().getCurrentTab());
-//        Log.d("LOG", "CurrentTab "+getTabHost().getCurrentTab());
-//        songspec.setContent(songsIntent);
-//
-//        // Tab for Videos
-//        TabSpec videospec = tabHost.newTabSpec("C");
-//        videospec.setIndicator("Videos", getResources().getDrawable(R.drawable.icon_videos_tab));
-//        Intent videosIntent = new Intent(this, C_activity.class);
-//        videosIntent.putExtra("position", getTabHost().getCurrentTab());
-//        Log.d("LOG", "CurrentTab "+getTabHost().getCurrentTab());
-//        videospec.setContent(videosIntent);
-//
-//        // Adding all TabSpec to TabHost
-//        tabHost.addTab(photospec); // Adding photos tab
-//        tabHost.addTab(songspec); // Adding songs tab
-//        tabHost.addTab(videospec); // Adding videos tab
-//    }
-//    
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
+
+	// public void getTabs() {
+	// // TODO Auto-generated method stub
+	//
+	// TabHost tabHost = getTabHost();
+	//
+	// // Tab for Photos
+	// TabSpec photospec = tabHost.newTabSpec("A");
+	// // setting Title and Icon for the Tab
+	// photospec.setIndicator("Photos",
+	// getResources().getDrawable(R.drawable.icon_photos_tab));
+	// Intent photosIntent = new Intent(this, A_activity.class);
+	// Log.d("LOG", "CurrentTab "+getTabHost().getCurrentTab());
+	// photosIntent.putExtra("position",
+	// getTabHost().getTabWidget().getChildCount());
+	// photospec.setContent(photosIntent);
+	//
+	// // Tab for Songs
+	// TabSpec songspec = tabHost.newTabSpec("B");
+	// songspec.setIndicator("Songs",
+	// getResources().getDrawable(R.drawable.icon_songs_tab));
+	// Intent songsIntent = new Intent(this, B_activity.class);
+	// songsIntent.putExtra("position", getTabHost().getCurrentTab());
+	// Log.d("LOG", "CurrentTab "+getTabHost().getCurrentTab());
+	// songspec.setContent(songsIntent);
+	//
+	// // Tab for Videos
+	// TabSpec videospec = tabHost.newTabSpec("C");
+	// videospec.setIndicator("Videos",
+	// getResources().getDrawable(R.drawable.icon_videos_tab));
+	// Intent videosIntent = new Intent(this, C_activity.class);
+	// videosIntent.putExtra("position", getTabHost().getCurrentTab());
+	// Log.d("LOG", "CurrentTab "+getTabHost().getCurrentTab());
+	// videospec.setContent(videosIntent);
+	//
+	// // Adding all TabSpec to TabHost
+	// tabHost.addTab(photospec); // Adding photos tab
+	// tabHost.addTab(songspec); // Adding songs tab
+	// tabHost.addTab(videospec); // Adding videos tab
+	// }
+	//
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
 }
